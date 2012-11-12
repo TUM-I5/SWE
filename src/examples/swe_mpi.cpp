0f5fc374b26dd4b734e1fd498ae329df55dfc99f
Sebastian Rettenberger <rettenbs@in.tum.de>
2012-11-12 15:46:25 +0100

    Use ASAGI_INPUT_DIR in MPI version

--- src/examples/swe_mpi.cpp
+++ src/examples/swe_mpi.cpp
@@ -201,8 +201,8 @@ int main( int argc, char** argv ) {
   simulationArea[2] = -2450000;
   simulationArea[3] = 1450000;
 
-  SWE_AsagiScenario l_scenario( "/work/breuera/workspace/geo_information/output/tohoku_gebco_ucsb3_500m_hawaii_bath.nc",
-                                "/work/breuera/workspace/geo_information/output/tohoku_gebco_ucsb3_500m_hawaii_displ.nc",
+  SWE_AsagiScenario l_scenario( ASAGI_INPUT_DIR "tohoku_gebco_ucsb3_500m_hawaii_bath.nc",
+		  	  	  	  	  	  	ASAGI_INPUT_DIR "tohoku_gebco_ucsb3_500m_hawaii_displ.nc",
                                 (float) 14400., simulationArea);
   #else
   // create a simple artificial scenario
