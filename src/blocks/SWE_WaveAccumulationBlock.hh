7055bc85237033ac20a0b8efb17c1f193ac97401
Michael Bader <bader@in.tum.de>
2014-01-02 17:03:35 +0100

    Added class SWE_Wave_AccumulationBlock: same numerics as SWE_WavePropagationBlock, but changed implementation w.r.t. to storage of net updates.

---
+++ src/blocks/SWE_WaveAccumulationBlock.hh
@@ -0,0 +1,112 @@
+/**
+ * @file
+ * This file is part of SWE.
+ *
+ * @author Alexander Breuer (breuera AT in.tum.de, http://www5.in.tum.de/wiki/index.php/Dipl.-Math._Alexander_Breuer)
+ * @author Sebastian Rettenberger (rettenbs AT in.tum.de, http://www5.in.tum.de/wiki/index.php/Sebastian_Rettenberger,_M.Sc.)
+ * @author Michael Bader (bader AT in.tum.de, http://www5.in.tum.de/wiki/index.php/Michael_Bader)
+ *
+ * @section LICENSE
+ *
+ * SWE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SWE.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @section DESCRIPTION
+ *
+ * SWE_Block, which uses solvers in the wave propagation formulation.
+ */
+
+#ifndef SWEWAVEPROPAGATIONBLOCK_HH_
+#define SWEWAVEPROPAGATIONBLOCK_HH_
+
+#include "blocks/SWE_Block.hh"
+#ifdef DYNAMIC_DISPLACEMENTS
+#include "scenarios/Asagi.hpp"
+#endif
+#include "tools/help.hh"
+
+#include <string>
+
+//SWE_WavePropagationBlock only support the following wave propagation solvers:
+//  2: Approximate Augmented Riemann solver (functional implementation: AugRieFun)
+//  4: f-Wave (vectorized implementation: FWaveVec) 
+#if WAVE_PROPAGATION_SOLVER==2
+#include "solvers/AugRieFun.hpp"
+#elif WAVE_PROPAGATION_SOLVER==4
+#include "solvers/FWaveVec.hpp"
+#else
+#error chosen wave propagation solver not supported by SWE_WavePropagationBlock
+#endif
+
+/**
+ * SWE_WavePropagationBlock is an implementation of the SWE_Block abstract class.
+ * It uses a wave propagation solver which is defined with the pre-compiler flag WAVE_PROPAGATION_SOLVER (see above).
+ *
+ * Possible wave propagation solvers are:
+ *  F-Wave, Apprximate Augmented Riemann, Hybrid (f-wave + augmented).
+ *  (details can be found in the corresponding source files)
+ */
+class SWE_WavePropagationBlock: public SWE_Block {
+
+#if WAVE_PROPAGATION_SOLVER==2
+    //! Approximate Augmented Riemann solver
+    solver::AugRieFun<float> wavePropagationSolver;
+#elif WAVE_PROPAGATION_SOLVER==4
+    //! Vectorized FWave solver
+    solver::FWaveVec<float> wavePropagationSolver;
+#else
+#error This implementation of SWE_WavePropagationBlock only supports WAVE_PROPAGATION_SOLVER==2 (AugRie) and WAVE_PROPAGATION_SOLVER==4 (fWaveVec)
+#endif
+
+    //! net-updates for the heights of the cells (for accumulation)
+    Float2D hNetUpdates;
+
+    //! net-updates for the x-momentums of the cells (for accumulation)
+    Float2D huNetUpdates;
+
+    //! net-updates for the y-momentums of the cells (for accumulation)
+    Float2D hvNetUpdates;
+
+  public:
+    //constructor of a SWE_WavePropagationBlock.
+    SWE_WavePropagationBlock(int l_nx, int l_ny,
+    					float l_dx, float l_dy);
+
+    //executes a single timestep.
+    virtual void simulateTimestep(float dt);
+
+    //computes the net-updates for the block
+    void computeNumericalFluxes();
+
+    //update the cells
+    void updateUnknowns(float dt);
+    void updateUnknownsRow(float dt, int i);
+
+    //runs the simulation until i_tEnd is reached.
+    float simulate(float i_tStart, float i_tEnd);
+
+    //updates the bathymetry with the current displacment values
+#ifdef DYNAMIC_DISPLACEMENTS
+    bool updateBathymetryWithDynamicDisplacement(scenarios::Asagi &i_asagiScenario, float time);
+#endif
+
+    /**
+     * Destructor of a SWE_WavePropagationBlock.
+     *
+     * In the case of a hybrid solver (NDEBUG not defined) information about the used solvers will be printed.
+     */
+    virtual ~SWE_WavePropagationBlock() {}
+};
+
+#endif /* SWEWAVEPROPAGATIONBLOCK_HH_ */
