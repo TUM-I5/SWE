f71cbc639cf13cb7b22c4cacccc97e65447bfa57
Marc Marot-Lassauzaie <marot.marc@tum.de>
2024-09-12 10:23:23 +0200

    removed legacy netcdf interfaces

--- CMake/FindNetCDF.cmake
+++ CMake/FindNetCDF.cmake
@@ -36,7 +36,7 @@ find_path (NETCDF_INCLUDES netcdf.h
 find_library (NETCDF_LIBRARIES_C       NAMES netcdf)
 mark_as_advanced(NETCDF_LIBRARIES_C)
 
-set (NetCDF_has_interfaces "YES") # will be set to NO if we're missing any interfaces
+
 set (NetCDF_libs "${NETCDF_LIBRARIES_C}")
 
 get_filename_component (NetCDF_lib_dirs "${NETCDF_LIBRARIES_C}" PATH)
@@ -51,7 +51,6 @@ macro (NetCDF_check_interface lang header libs)
     if (NETCDF_INCLUDES_${lang} AND NETCDF_LIBRARIES_${lang})
       list (INSERT NetCDF_libs 0 ${NETCDF_LIBRARIES_${lang}}) # prepend so that -lnetcdf is last
     else (NETCDF_INCLUDES_${lang} AND NETCDF_LIBRARIES_${lang})
-      set (NetCDF_has_interfaces "NO")
       message (STATUS "Failed to find NetCDF interface for ${lang}")
     endif (NETCDF_INCLUDES_${lang} AND NETCDF_LIBRARIES_${lang})
   endif (NETCDF_${lang})
@@ -66,6 +65,6 @@ set (NETCDF_LIBRARIES "${NetCDF_libs}" CACHE STRING "All NetCDF libraries requir
 # handle the QUIETLY and REQUIRED arguments and set NETCDF_FOUND to TRUE if
 # all listed variables are TRUE
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (NetCDF DEFAULT_MSG NETCDF_LIBRARIES NETCDF_INCLUDES NetCDF_has_interfaces)
+find_package_handle_standard_args (NetCDF DEFAULT_MSG NETCDF_LIBRARIES NETCDF_INCLUDES)
 
 mark_as_advanced (NETCDF_LIBRARIES NETCDF_INCLUDES)
